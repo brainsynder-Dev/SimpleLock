9173256172e7bc8cbd7da0e3cc13ca804eeb46d3
brainsynder <briansnyder498@gmail.com>
2018-06-02 23:29:10 -0400

    (WIP) Added the InteractListener to check for when a player interacts with a protected block/sign

--- src/main/java/lock/brainsynder/Core.java
+++ src/main/java/lock/brainsynder/Core.java
@@ -1,7 +1,11 @@
 package lock.brainsynder;
 
+import lock.brainsynder.listeners.InteractListener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Core extends JavaPlugin {
-
+    @Override
+    public void onEnable() {
+        getServer().getPluginManager().registerEvents(new InteractListener(this), this);
+    }
 }
