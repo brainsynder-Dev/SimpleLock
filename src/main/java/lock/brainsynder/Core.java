6065c3fd6a0084ffdce47d8d8a02eabca1530320
brainsynder <briansnyder498@gmail.com>
2018-06-05 15:31:54 -0400

    Inserted missing code in the Core.class

--- src/main/java/lock/brainsynder/Core.java
+++ src/main/java/lock/brainsynder/Core.java
@@ -2,12 +2,30 @@ package lock.brainsynder;
 
 import lock.brainsynder.listeners.BlockListener;
 import lock.brainsynder.listeners.InteractListener;
+import lock.brainsynder.storage.StorageMaker;
+import lock.brainsynder.utils.ProtectionUtils;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.io.File;
+
 public class Core extends JavaPlugin {
+    private StorageMaker storage;
+
+
     @Override
     public void onEnable() {
+        storage = new StorageMaker(new File(getDataFolder(), "SignStorage.stc"));
+        ProtectionUtils.loadProtection(this);
         getServer().getPluginManager().registerEvents(new BlockListener(), this);
         getServer().getPluginManager().registerEvents(new InteractListener(this), this);
+    }
+
+    @Override
+    public void onDisable() {
+        ProtectionUtils.saveProtections(this);
+    }
+
+    public StorageMaker getStorage() {
+        return storage;
     }
 }
