d1d45ab0b21a086d9d90fb78e111d35fbab4a31d
brainsynder <briansnyder498@gmail.com>
2018-06-14 13:01:14 -0400

    Fixed spacing issue

--- src/main/java/lock/brainsynder/utils/DoubleChestInfo.java
+++ src/main/java/lock/brainsynder/utils/DoubleChestInfo.java
@@ -7,51 +7,51 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class DoubleChestInfo {
-        private Map<String, Block> sideMap;
+    private Map<String, Block> sideMap;
 
-        private DoubleChestInfo() {
-            sideMap = new HashMap<>();
-        }
+    private DoubleChestInfo() {
+        sideMap = new HashMap<>();
+    }
 
-        static DoubleChestInfo newInstance() {
-            return new DoubleChestInfo();
-        }
+    static DoubleChestInfo newInstance() {
+        return new DoubleChestInfo();
+    }
 
-        void setBlock(Side side, Block block) {
-            sideMap.put(side.name(), block);
-        }
+    void setBlock(Side side, Block block) {
+        sideMap.put(side.name(), block);
+    }
 
-        void setLocation(Side side, Location location) {
-            sideMap.put(side.name(), location.getBlock());
-        }
+    void setLocation(Side side, Location location) {
+        sideMap.put(side.name(), location.getBlock());
+    }
 
-        public Block getBlock(Side side) {
-            if (sideMap.containsKey(side.name())) {
-                return sideMap.get(side.name());
-            }
-            return null;
+    public Block getBlock(Side side) {
+        if (sideMap.containsKey(side.name())) {
+            return sideMap.get(side.name());
         }
+        return null;
+    }
 
-        public Location getLocation(Side side) {
-            if (sideMap.containsKey(side.name())) {
-                return sideMap.get(side.name()).getLocation();
-            }
-            return null;        
+    public Location getLocation(Side side) {
+        if (sideMap.containsKey(side.name())) {
+            return sideMap.get(side.name()).getLocation();
         }
-        
-        public Side getSide (Location location) {
-            for (Map.Entry<String, Block> sideInfo : sideMap.entrySet()) {
-                Location loc = sideInfo.getValue().getLocation();
-                
-                if (loc.getWorld().getName().equals(location.getWorld().getName())) {
-                    if ((loc.getBlockX() == location.getBlockX()) 
-                            || (loc.getBlockY() == location.getBlockY())
-                            || (loc.getBlockZ() == location.getBlockZ())) {
-                        return Side.valueOf(sideInfo.getKey());
-                    }
+        return null;
+    }
+
+    public Side getSide(Location location) {
+        for (Map.Entry<String, Block> sideInfo : sideMap.entrySet()) {
+            Location loc = sideInfo.getValue().getLocation();
+
+            if (loc.getWorld().getName().equals(location.getWorld().getName())) {
+                if ((loc.getBlockX() == location.getBlockX())
+                        || (loc.getBlockY() == location.getBlockY())
+                        || (loc.getBlockZ() == location.getBlockZ())) {
+                    return Side.valueOf(sideInfo.getKey());
                 }
             }
-            
-            return Side.RIGHT;
         }
+
+        return Side.RIGHT;
     }
+}
